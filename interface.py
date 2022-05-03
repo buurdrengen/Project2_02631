7f724f27174d0cb7ac309af71f1c1406479fc7c9
cglitz-dtu <cglitz@biosustain.dtu.dk>
2022-05-03 11:51:59 +0200

    interface modified

--- interface.py
+++ interface.py
@@ -8,11 +8,10 @@ Created on Thu Mar 31 09:18:44 2022
 import numpy as np
 # imports three-dimensional array with Nx, Ny, Nz
 from loaddata import dataLoad
-
 from input_number import inputNumber
 from input_number_integers import inputNumber_int
 from displayMenu import displayMenu
-from statistics import dataStatistics
+from statistics_2 import dataStatistics
 #from dataPlot import dataPlot
 from inputFilename import inputFilename
 
@@ -54,39 +53,27 @@ while True:
             choice_statistics = displayMenu(statistics_menuItems)
             if choice_statistics == 4:
                 break
-            Yref = None 
+            Yref = None
             Zref = None
-            DeltaX = None 
+            DeltaX = None
         #compute statistics via dataStatistics function 
-<<<<<<< Updated upstream
-        # test = dataStatistics(data, statistics_menuItems[choice_statistics-1])
-        # print(test)
         # Remember to display y- and z-coordinates!! 
-            if choice_statistics == 1:
-                dataStatistics(data,"Mean")
-                print('Mean temperature = {:.2f} °C'.format(dataStatistics(data, 'mean temperature')),"\n")
-=======
             # returns mean of user determined y and z coordinate. Input number has to be integer, otherwise error message is displayed 
             if choice_statistics == 1:
                 # mean_array = 2D array (z to y dimension) with all the means through x-dimension
                 mean_array = dataStatistics(data,"Mean",Yref,Zref,DeltaX)
-                z_coordinate = inputNumber_int('Please input the z_coordinate you want to display the mean for: ')
-                y_coordinate = inputNumber_int('Please input the y_coordinate you want to display the mean for: ')
+                # => because -1 of index compared to dimension given as input before 
+                while z_coordinate => dimension_z:
+                    z_coordinate = inputNumber_int('Please input the z_coordinate you want to display the mean for: ')
+                while y_coordinate => dimension_y:
+                    y_coordinate = inputNumber_int('Please input the y_coordinate you want to display the mean for: ')
                 # mean of input coordinates 
                 mean = mean_array[z_coordinate, y_coordinate]
                 print('Mean = {:.2f}'.format(mean,"\n"))
->>>>>>> Stashed changes
+                print('')
                 continue
             # returns variance of user determined y and z coordinate. Input number has to be integer, otherwise error message is displayed 
-            elif choice_statistics == 2:
-<<<<<<< Updated upstream
-                dataStatistics(data, "Variance")
-                print('Mean growth rate = {:.2f}'.format(dataStatistics(data, 'mean growth rate')),"\n")
-                continue
-            elif choice_statistics ==3:
-                dataStatistics(data, "Cross correlation")
-                print('Standard temperature = {:.2f}'.format(dataStatistics(data, 'std temperature')),"\n")
-=======
+            elif choice_statistics == 2: 
                 # 2D array (z to y dimension) with all variances through x-dimesnion
                 variance_array = dataStatistics(data, "Variance",Yref,Zref,DeltaX)
                 z_coordinate = inputNumber_int('Please input the z_coordinate you want to display the variance for: ')
@@ -94,14 +81,16 @@ while True:
                 # variance for input coordinates 
                 variance = variance_array[z_coordinate, y_coordinate]
                 print('Variance = {:.2f}'.format(variance,"\n"))
+                print("")
                 continue
+            
             elif choice_statistics == 3:
-                Yref = inputNumber_int('Please input the y_coordinate for the cross correlation: ')
-                Zref = inputNumber_int('Please input the z_coordinate for the cross correlation: ')
+                Yref = inputNumber_int('Please input the y_coordinate (Yref) for the cross correlation: ')
+                Zref = inputNumber_int('Please input the z_coordinate (Zref) for the cross correlation: ')
                 DeltaX = inputNumber_int('Please input the separation in x-coordinate for which the cross-correlation shall be evaluated for: ')
                 cross_correlation = dataStatistics(data, "Cross correlation", Yref, Zref, DeltaX)
-                print('Cross correlation = {:.2f}'.format(cross_correlation,"\n"))
->>>>>>> Stashed changes
+                print(cross_correlation)
+                print('')
                 continue
                            
     # ------------------------------------------------------------------
